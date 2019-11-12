c8fd2b8cfe6d2790e876e95b2799fb718cee2bcb
“juanmndz” <juanmndz19@gmail.com>
2019-11-12 10:24:25 -0700

    Mention additon

--- src/decorators/Mention/Mention/index.js
+++ src/decorators/Mention/Mention/index.js
@@ -1,46 +1,88 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import './styles.css';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import "./styles.css";
 
 class Mention {
   constructor(className) {
     this.className = className;
   }
+  renderMentionData = (description, status, value) => {
+    // console.log(data, 'data')
+    if (status === "critical") {
+      return (
+        <a className="tooltipx critical">
+          {value}
+          <span className="custom critical">
+            <img
+              src="https://rettex-images.s3.amazonaws.com/Critical.png"
+              alt="Error"
+              height="48"
+              width="48"
+            />
+            <em>Critical</em>${description}
+          </span>
+        </a>
+      );
+    }
+    if (status === "info") {
+      return (
+        <a className="tooltipx help">
+          {value}
+          <span className="custom help">
+            <img
+              src="https://rettex-images.s3.amazonaws.com/Help.png"
+              alt="Help"
+              height="48"
+              width="48"
+            />
+            <em>Help</em>
+            {description}
+          </span>
+        </a>
+      );
+    }
+    return value;
+  };
   getMentionComponent = () => {
     const className = this.className;
     const MentionComponent = ({ entityKey, children, contentState }) => {
-      const { url, value } = contentState.getEntity(entityKey).getData();
+      const { description, status, value } = contentState
+        .getEntity(entityKey)
+        .getData();
+      const renderMention = this.renderMentionData(description, status, value);
       return (
-        <a href={url || value} className={classNames('rdw-mention-link', className)}>
+        <React.Fragment>
+          {renderMention}
           {children}
-        </a>
+        </React.Fragment>
       );
     };
     MentionComponent.propTypes = {
       entityKey: PropTypes.number,
       children: PropTypes.array,
-      contentState: PropTypes.object,
+      contentState: PropTypes.object
     };
     return MentionComponent;
   };
   getMentionDecorator = () => ({
     strategy: this.findMentionEntities,
-    component: this.getMentionComponent(),
+    component: this.getMentionComponent()
   });
 }
 
-Mention.prototype.findMentionEntities = (contentBlock, callback, contentState) => {
-  contentBlock.findEntityRanges(
-    (character) => {
-      const entityKey = character.getEntity();
-      return (
-        entityKey !== null &&
-        contentState.getEntity(entityKey).getType() === 'MENTION'
-      );
-    },
-    callback,
-  );
+Mention.prototype.findMentionEntities = (
+  contentBlock,
+  callback,
+  contentState
+) => {
+  contentBlock.findEntityRanges(character => {
+    const entityKey = character.getEntity();
+    return (
+      entityKey !== null &&
+      contentState.getEntity(entityKey).getType() === "MENTION"
+    );
+  }, callback);
 };
 
 module.exports = Mention;
