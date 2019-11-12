e42d3c49bd10e892d300e42478eb49630dc9dada
“juanmndz” <juanmndz19@gmail.com>
2019-11-12 10:32:35 -0700

    Add build

--- src/decorators/Mention/Mention/index.js
+++ src/decorators/Mention/Mention/index.js
@@ -54,7 +54,7 @@ class Mention {
       return (
         <React.Fragment>
           {renderMention}
-          {children}
+          {/* {children} */}
         </React.Fragment>
       );
     };
